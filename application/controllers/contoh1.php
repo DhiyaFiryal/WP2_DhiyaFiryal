ba8fccada471975bd67e3ea07b392d112c6b7158
dhiya <dhiyaafiryall@gmail.com>
2024-03-26 12:52:47 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends C1_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Dhiya Firyal
+            Saya tinggal di Bekasi Timur
+            olahraga yang saya sukai adalah Renang";
+    }
+}
