00ff1643f7caccababfb666849bc77eb13ae9604
dhiya <dhiyaafiryall@gmail.com>
2024-04-02 16:50:20 +0700

    Tugas Pertemuan 2

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dhiya </li>
+        <li>Nama Belakang : Firyal </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl Taman Bumyagara Blok G2 NO 24</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Renang</li>
+        <li>Bersepeda</li>
+    </ul>
+</section>
